485816bc7c821d58e39b9ca6e3af6e7345fcc1f5
Sahasra-yalamanchali <24wh1a0450@bvrithyderabad.edu.in>
2026-07-11 08:49:40 +0530

    Initial Spring Boot project setup
    Added Spring Boot project with MySQL configuration and required dependencies.

---
+++ src/test/java/com/smartcity/smartcityguide/SmartCityGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcity.smartcityguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
